4c5dbc20a5cf5d9aae0a140a232a336185a7089f
djbberko <daniel@envoyathome.com>
2022-03-18 13:16:11 -0400

    bunnyHops cleanup

--- challenge2.py
+++ challenge2.py
@@ -5,7 +5,8 @@
 # Say n == 3. The number of paths to 3 represents the number of distinct ways that the bunny can hop to reach 3 steps. res = 4.
 # Notice the overlapping sub-problems. Why compute the sub tree for 2 twice? Use DP.
 # dp[i], distinct number of ways to reach the ith step
-# dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+# Base Case: n == 0: 0, n == 1: 1, n == 2: 2, n == 3: 4
+# Recursive Case: dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
 def bunnyHops(n):
     if n < 0:
@@ -19,6 +20,20 @@ def bunnyHops(n):
     if n == 3:
         return 4
 
+    def topDown():
+        dp = {}
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+
+        def dfs(n):
+            if n in dp:
+                return dp[n]
+            dp[n] = dfs(n-1) + dfs(n-2) + dfs(n-3)
+            return dp[n]
+        return dfs(n)
+
     def bottomUp():
         dp = [0] * (n + 1)
         dp[0] = 0
@@ -28,6 +43,7 @@ def bunnyHops(n):
         for i in range(4, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
         return dp[-1]
+
     return bottomUp()
 
 
@@ -36,8 +52,10 @@ def main():
     assert(bunnyHops(0) == 0)
     assert(bunnyHops(1) == 1)
     assert(bunnyHops(2) == 2)
+    assert(bunnyHops(3) == 4)
     assert(bunnyHops(4) == 7)
     assert(bunnyHops(5) == 13)
+    assert(bunnyHops(6) == 24)
     return
 
 
